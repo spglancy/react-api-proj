6938cf2b23aca38ea6b2b6c678722eb8e0f82915
Sean <Spglancy@gmail.com>
2019-02-10 19:46:30 -0800

    forgot to commit all my work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import Button from './Button'
 
 import './App.css';
 
@@ -25,96 +26,81 @@ class App extends Component {
     super(props)
 
     this.state = {
-      inputValue: '',     // Used to hold value entered in the input field
-      weatherData: null,  // Used to hold data loaded from the weather API
+      charData: null,  // Used to hold data loaded from the rick and morty API
+      page: 1
     }
   }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    // ! Get your own API key ! 
-    const apikey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
-    // Get the zip from the input
-    const zip = this.state.inputValue
-    // Form an API request URL with the apikey and zip
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apikey}`
+  // fetchData() {
+  //   const url = `https://rickandmortyapi.com/api/character/?page=${this.state.page}`
+  //   // Get data from the API with fetch
+  //   fetch(url).then(res => {
+  //     // Handle the response stream as JSON
+  //     return res.json()
+  //   }).then((json) => {
+  //     console.log("we got data")
+  //     // If the request was successful assign the data to component state
+  //     this.setState({ charData: json })
+  //   }).catch((err) => {
+  //     // If there is no data 
+  //     this.setState({ charData: null }) // Clear the character data we don't have any to display
+  //     // Print an error to the console. 
+  //     console.log('-- Error fetching --')
+  //     console.log(err.message)
+  //     // You may want to display an error to the screen here. 
+  //   })
+  // }
+
+  handleClick(condition) {
+    let pageNum = this.state.page
+    condition === "forward" ? this.setState({ page: pageNum += 1 }): this.setState({ page: pageNum -= 1})
+  }
+
+  renderChar() {
+    const url = `https://rickandmortyapi.com/api/character/?page=${this.state.page}`
     // Get data from the API with fetch
     fetch(url).then(res => {
       // Handle the response stream as JSON
       return res.json()
     }).then((json) => {
+      console.log("we got data")
       // If the request was successful assign the data to component state
-      this.setState({ weatherData: json })
-      // ! This needs better error checking here or at renderWeather() 
-      // It's possible to get a valid JSON response that is not weather 
-      // data, for example when a bad zip code entered.
+      this.setState({ charData: json })
     }).catch((err) => {
       // If there is no data 
-      this.setState({ weatherData: null }) // Clear the weather data we don't have any to display
+      this.setState({ charData: null }) // Clear the character data we don't have any to display
       // Print an error to the console. 
       console.log('-- Error fetching --')
       console.log(err.message)
       // You may want to display an error to the screen here. 
     })
-  }
-
-  renderWeather() {
-    // This method returns undefined or a JSX component
-    if (this.state.weatherData === null) {
-      // If there is no data return undefined
+    if (this.state.charData === null) {
       return undefined
     }
-
-    /* 
-    This next step needs another level of error checking. It's 
-    possible to get a JSON response for an invalid zip in which 
-    case the step below fails. 
-    */ 
-    console.log(this.state.weatherData)
-    // Take the weather data apart to more easily populate the component
-    const { main, description, icon } = this.state.weatherData.weather[0]
-    const { temp, pressure, humidity, temp_min, temp_max } = this.state.weatherData.main 
-    
-    return (
-      <div>
-        <div>Title: {main}</div>
-        <div>Desc: {description}</div>
-        <div>Icon: {icon}</div>
-        <div>Temp: {temp}</div>
-        <div>Pressure: {pressure}</div>
-        <div>Humidity: {humidity}</div>
-        <div>Temp Min: {temp_min} Max:{temp_max}</div>
-      </div>
-    )
-  }
+    return this.state.charData.results.map(({ name, status, species, type, gender, image }) => {
+      if(type === ""){
+        type = "No Type Specified"
+      }
+     return( <div className = "character">
+              <img alt = "placeholder img" src = {image}></img>
+              <div>Name: {name}</div>
+              <div>Status: {status}</div>
+              <div>Species: {species}</div>
+              <div>Type: {type}</div>
+              <div>Gender: {gender}</div>
+           </div>
+     )}
+    )}
 
   render() {
     return (
       <div className="App">
-
-        {/** This input uses the controlled component pattern */}
-        <form onSubmit={e => this.handleSubmit(e)}>
-
-          {/** 
-          This pattern is used for input and other form elements 
-          Set the value of the input to a value held in component state
-          Set the value held in component state when a change occurs at the input 
-          */}
-          <input 
-            value={this.state.inputValue} 
-            onChange={e => this.setState({ inputValue: e.target.value })}
-            type="text" 
-            pattern="(\d{5}([\-]\d{4})?)"
-            placeholder="enter zip"
-          />
-
-          <button type="submit">Submit</button>
-
-        </form>
-
-        {/** Conditionally render this component */}
-        {this.renderWeather()}
-
+        <div className = "charContainer">{this.renderChar()}</div>
+        <div className = "buttonContainer">
+          <Button name = "back" className = "button" onClick = { () => this.handleClick("back") } />
+          <span>Page: { this.state.page }</span>
+          <Button name = "forward" className = "button" onClick = { () => this.handleClick("forward") } />
+        </div>
       </div>
     );
   }
